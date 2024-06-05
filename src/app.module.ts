2203e75318f315255856f646114246f172d98ae4
Matheus Souza dos santos <msdsantos96@gmail.com>
2024-06-05 02:51:00 -0300

    chore: config docker and typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({
+    type: 'postgres', // ou 'mysql', 'sqlite', etc.
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORTA), // ajuste a porta conforme necessário
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    migrationsTableName: 'migrations', // Nome da tabela de migrações
+    migrations: [__dirname + '/migrations/*{.ts,.js}'], // Diretório de migrações
+    synchronize: false, // Defina como false para não sincronizar automaticamente as entidades com o banco de dados
+  }),],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+}
